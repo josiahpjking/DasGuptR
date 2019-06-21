2fb1ff26c942a599ac9c084e9f99c7614eb074fa
Josiah King <josiahpjking@gmail.com>
2019-06-21 12:47:52 +0100

    comment out quickly

--- R/DasGupt_2pop.R
+++ R/DasGupt_2pop.R
@@ -1,5 +1,5 @@
-#' Standardisation and decomposition of rates over P rate-factors and 2 populations. 
-#' Outputs a ? object 
+#' Standardisation and decomposition of rates over P rate-factors and 2 populations.
+#' Outputs a ? object
 #' @param df dataframe or tibble object, with columns specifying 1) population and 2) each rate-factor to be considered
 #' @param pop tidyselect variable indicating population ID
 #' @param factrs character vector of rate-factors
@@ -7,7 +7,7 @@
 #' @examples
 #' ......
 DasGupt_2pop<-function(df,pop,factrs){
-  
+
   pop=enquo(pop)
   nfact=length(factrs)
   print(factrs)
@@ -21,20 +21,20 @@ DasGupt_2pop<-function(df,pop,factrs){
       pop_prods=map(factor_mat,rowProds) #calculate total products (can simply divide by \alpha afterwards)
     ) -> df_nested
 
-  #the get_effect function will loop over the indices of your factors, and for 
+  #the get_effect function will loop over the indices of your factors, and for
   #each factor A, it will get the BCD... adjusted rates for each population, and
   #spit out the difference too.
   #equivalent to Q1, Q2, ....  in Ben's function
   decomp_out<-suppressMessages(map(1:nfact,~DGadjust_ratefactor(df_nested,pop,.,factrs)))
   names(decomp_out)<-factrs
-  
-  decomp_out <-
-  decomp_out %>%
-    tibble(
-      factor = names(.),
-      results = .
-    ) %>%
-    unnest()
-  
+
+  # decomp_out <-
+  # decomp_out %>%
+  #   tibble(
+  #     factor = names(.),
+  #     results = .
+  #   ) %>%
+  #   unnest()
+
   return(decomp_out)
 }
